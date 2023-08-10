a70cfd8974dd0087ee9bb8dec717d7fbd061fab9
imeshkirinda <89736765+imeshkirinda@users.noreply.github.com>
2023-08-10 18:57:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -30,7 +30,7 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
 global.vcardowner = ['+94772229832'] // owner number
-global.ownername = "👑 KING IMESH❯❯" // owner name
+global.ownername = "👑 KING IMESH" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
